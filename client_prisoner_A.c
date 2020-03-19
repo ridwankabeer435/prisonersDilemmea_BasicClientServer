ef218b03bc41f51e31cecbf61318b5c521f0c409
ridwankabeer435 <ridwankabeer@gmail.com>
2020-03-18 22:48:49 -0300

    master branch now includes code from trialcase version

--- client_prisoner_A.c
+++ client_prisoner_A.c
@@ -8,46 +8,71 @@
 #include <netdb.h>
 #include <arpa/inet.h>
 
-#define PORTNUM 3030;
-
-void validateUserInput(char input[]){
-
-
-}
+#define PORTNUM 8000
 
 int main(){
 
-  //get input and process it
-  char userInput[800]; //this will serve as the inpi
+	//get input and process it
+	char userInput[250]; //this will serve as the input
 
-  //validate user input
+	printf("Enter a value: ");
+	fgets(userInput, sizeof(userInput), stdin);
 
-  //if valid, then proceed to connect with server
+	//validate user input
+	while(userInput[0]!='B' && userInput[0]!='S'){
+		printf("Wrong data entry. Please re-enter: ");
+		fgets(userInput, sizeof(userInput), stdin);
+	}
 
+ 	//if valid, then proceed to connect with server
+  printf("You entered: ");
+  puts(userInput);
 
   //while connected to server, listen for responses from server.
 
   //create the socket: use socket()
-  //
-  int clientServerSocket = socket(PF_INET, SOCKET_STREAM, 0);
+
+  int clientServerSocket = socket(PF_INET, SOCK_STREAM, 0);
+
+  if(clientServerSocket < 0){
+    fprintf(stderr, "%s", "Connection socket is not working...\n");
+  }
 
   //like at server, assign address to server for socket to connect
   struct sockaddr_in serverAddress;
   serverAddress.sin_family = AF_INET;
-  serverAddress.sin_port = PORTNUM;
-  serverAddress.sin_addr.s_addr = INADDR_ANY;
+  serverAddress.sin_port = htons(PORTNUM);
+  serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1");
 
- //connect client via socket
- int clientConnect = connect(clientServerSocket, (struct sockaddr *) &serverAddress, sizeof(serverAddress));
 
- //write the user's data to the server: use 'write()' functtion
- write(clientServerSocket, userInput, strlen(userInput));
 
- //read the server's responseL use 'read()' function
-read(clientServerSocket, userInput, sizeof(userInput));
- //print out the response
- //terminate the connection
+  //connect client via socket
+  int clientConnect = connect(clientServerSocket, (struct sockaddr *) &serverAddress, sizeof(serverAddress));
 
- close(clientServerSocket);
+
+
+	if(clientConnect < 0){
+    fprintf(stderr, "%s", "error connecting to server\n" );
+  }
+  //write the user's data to the server: use 'write()' functtion
+  int n= write(clientServerSocket,userInput,strlen(userInput));
+  if(n < 0){
+    fprintf(stderr, "%s", "Error sending data to server\n");
+  }
+
+  bzero(userInput, sizeof(userInput));
+
+
+  //read the server's responseL use 'read()' function
+  n = read(clientServerSocket, userInput, sizeof(userInput));
+  if(n < 1){
+    fprintf(stderr, "%s", "Error receiving data from server\n");
+  }
+  //print out the response
+  printf("%s", userInput);
+
+  //terminate the connection
+  close(clientServerSocket);
+
   return 0;
 }
--- server_prisoner_B.c
+++ server_prisoner_B.c
@@ -9,8 +9,8 @@
 #include <arpa/inet.h>
 
 //declare a port number to communicate server
-#define PORTNUM 3030;
-#define MAXBACKLOG 5;
+#define PORTNUM 8000
+#define MAXBACKLOG 5
 
 /*Process server's response inside this function*/
 void dilemmaOutcome(){
@@ -21,20 +21,29 @@ int main(){
 
   //have a struct to store IPv4 address (for server: maybe use the local address)
   struct sockaddr_in serverAddress;
+//  socklen_t clientDataLen;
 
   //create the  'socket()' system call
   // we are using TCP stream for client-sercer commumication, default protocol
-  int serverSocket = socket(PF_INET, SOCKET_STREAM, 0);
-
+  int serverSocket = socket(PF_INET, SOCK_STREAM, 0);
+  if(serverSocket < 0){
+    fprintf(stderr, "%s","ERROR opening socket\n");
+  }
   //assign values to the server address
+  bzero((char *) &serverAddress, sizeof(serverAddress));
+
   serverAddress.sin_family = AF_INET; //IPv4 family of addresses
   serverAddress.sin_port = htons(PORTNUM); //assign port number to listen for connection
-  serverAddress.sin_addr.s_addr = INADDR_ANY; //evaluates to local IP address: 0.0.0.0
+  serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1"); //evaluates to local IP address: 0.0.0.0
 
 
   //bind the socket 'serverSocket' with IP address specified in serverAddress struct
   //use 'bind()' function
-  bind(serverSocket, (struct sockaddr *) &serverAddress, sizeof(serverAddress));
+  int binding = bind(serverSocket, (struct sockaddr *) &serverAddress, sizeof(serverAddress));
+  if(binding < 0){
+    fprintf(stderr, "%s","Can't bind socket\n");
+  }
+
 
   //make server open for listening to requests
   //use 'listen()': params ==> 'socket' to listen at for connection requests
@@ -51,23 +60,31 @@ int main(){
 
     //finally, create the socket to establish connection between server and client
     //use the 'accept()' function
-    int connectionSocket = accecpt(serverSocket, (struct sockaddr *) &clientAddress, &clientAddrSize);
-
+    int connectionSocket = accept(serverSocket, (struct sockaddr *) &clientAddress, &clientAddrSize);
+    if(connectionSocket < 0){
+      fprintf(stderr, "%s","Can't accept client request\n");
+    }
 
     //wil need an input buffer to store client's data
     char clientInput[800];
-    memset(clientInput, '\0', sizeof(clientInput));
+    //memset(clientInput, '\0', sizeof(clientInput));
+    bzero(clientInput, sizeof(clientInput));
 
     //so the server will need to read data from connectionSocket
     //use the read() function
-    read(connectionSocket, clientInput, sizeof(clientInput));
+    int readData = read(connectionSocket, clientInput, sizeof(clientInput));
+    if(readData < 0){
+      fprintf(stderr, "%s","can't read data\n");
+    }
 
     //check user's data
 
     //process the server's response to the client
-
-    //send the response back to the client
-    write(connectionSocket, "You sent me something", sizeof(clientInput));
+    printf("This is what I got %s \n", clientInput);
+    int response =  write(connectionSocket,"You sent me something\n",200);
+    if(response < 0){
+      fprintf(stderr, "%s","Error responding to data\n");
+    }
     //once done with everything close the connection socket
     close(connectionSocket);
   }
