91d389c8e124f77e8c3504e7b4c84a5155ceed0a
Abdullah Al Mukaddim <33270655+mukaddim98@users.noreply.github.com>
2020-03-27 16:33:26 -0300

    Fixed indentations and comments

--- client_prisoner_A.c
+++ client_prisoner_A.c
@@ -22,12 +22,9 @@ int main(int argc, char *argv[]){
 	char userInput[500]; //this will serve as the input
 	char tryAgainPrompt[200];
 	int portno = atoi(argv[1]);
-	//have a while loop to allow user to try again
-
-
+	
+	//a while loop to allow user to try again
 	while(1){
-
-
 		printf("Betray(B) or silence(S): ");
 		fgets(userInput, sizeof(userInput), stdin);
 
@@ -37,58 +34,55 @@ int main(int argc, char *argv[]){
 			fgets(userInput, sizeof(userInput), stdin);
 		}
 
-
 		while(userInput[0]!='B' && userInput[0]!='S'){
 			printf("Choose between silence(S) and betray(B): ");
 			fgets(userInput, sizeof(userInput), stdin);
 		}
 
- 		//if valid, then proceed to connect with server
-  	printf("You entered: ");
-  	puts(userInput);
+		//if valid, then proceed to connect with server
+		printf("You entered: ");
+		puts(userInput);
 
-  	//while connected to server, listen for responses from server.
+		//while connected to server, listen for responses from server.
 
-  	//create the socket: use socket()
+		//create the socket: use socket()
 
-  	int clientServerSocket = socket(PF_INET, SOCK_STREAM, 0);
+		int clientServerSocket = socket(PF_INET, SOCK_STREAM, 0);
 
-  	if(clientServerSocket < 0){
-    	fprintf(stderr, "%s", "Connection socket is not working...\n");
-  	}
+		if(clientServerSocket < 0){
+			fprintf(stderr, "%s", "Connection socket is not working...\n");
+		}
 
-  	//like at server, assign address to server for socket to connect
-  	struct sockaddr_in serverAddress;
-  	serverAddress.sin_family = AF_INET;
-  	serverAddress.sin_port = htons(portno);
-  	serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1");
+		//like at server, assign address to server for socket to connect
+		struct sockaddr_in serverAddress;
+		serverAddress.sin_family = AF_INET;
+		serverAddress.sin_port = htons(portno);
+		serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-
-
-  	//connect client via socket
-  	int clientConnect = connect(clientServerSocket, (struct sockaddr *) &serverAddress, sizeof(serverAddress));
-
-
+		//connect client via socket
+		int clientConnect = connect(clientServerSocket, (struct sockaddr *) &serverAddress, sizeof(serverAddress));
 
 		if(clientConnect < 0){
-    	fprintf(stderr, "%s", "error connecting to server\n" );
-  	}
-  	//write the user's data to the server: use 'write()' functtion
-  	int n= write(clientServerSocket,userInput,strlen(userInput));
-  	if(n < 0){
-    	fprintf(stderr, "%s", "Error sending data to server\n");
-  	}
+			fprintf(stderr, "%s", "error connecting to server\n" );
+		}
+		
+		//write the user's data to the server: use 'write()' functtion
+		int n= write(clientServerSocket,userInput,strlen(userInput));
+		
+		if(n < 0){
+			fprintf(stderr, "%s", "Error sending data to server\n");
+		}
 
-  	bzero(userInput, sizeof(userInput));
+		bzero(userInput, sizeof(userInput));
 
-
-  	//read the server's responseL use 'read()' function
-  	n = read(clientServerSocket, userInput, sizeof(userInput));
-  	if(n < 1){
-    	fprintf(stderr, "%s", "Error receiving data from server\n");
-  	}
-  	//print out the response
-  	printf("%s", userInput);
+		//read the server's responseL use 'read()' function
+		n = read(clientServerSocket, userInput, sizeof(userInput));
+		if(n < 1){
+			fprintf(stderr, "%s", "Error receiving data from server\n");
+		}
+		
+		//print out the response
+		printf("%s", userInput);
 
 		//ask the user if they want to continue
 		printf("Would you like to try again (Y/N): ");
@@ -113,9 +107,8 @@ int main(int argc, char *argv[]){
 			close(clientServerSocket);
 			break;
 		}
-
 	}
-
-
+	
   return 0;
+
 }
